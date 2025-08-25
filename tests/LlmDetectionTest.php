ed59ca5654d921b5fe5cee3a69820a2ace8a27d1
Deuchnord <Deuchnord@users.noreply.github.com>
2025-08-25 18:33:06 +0200

    feat: block Diffbot IP addresses (#8)

--- tests/LlmDetectionTest.php
+++ tests/LlmDetectionTest.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpFoundation\Request;
 class LlmDetectionTest extends TestCase
 {
     /* @phpstan-ignore missingType.iterableValue */
-    public static function crawlerProvider(): iterable
+    public static function crawlerProviderUserAgents(): iterable
     {
         foreach (LlmDetection::CRAWLER_USER_AGENTS as [$crawler, $crawlerUserAgents]) {
             foreach ($crawlerUserAgents as $crawlerUserAgent) {
@@ -30,16 +30,56 @@ class LlmDetectionTest extends TestCase
         }
     }
 
-    #[DataProvider('crawlerProvider')]
-    public function testItDetectsLlmCrawlers(LlmCrawler $crawler, string $userAgent): void
+    /**
+     * @return list<array{LlmCrawler, list<string>}>
+     */
+    public static function crawlerProviderIpAddress(): array
+    {
+        return [
+            [LlmCrawler::DIFFBOT, ['64.62.212.42', '64.62.255.255', '64.71.132.1']],
+        ];
+    }
+
+    #[DataProvider('crawlerProviderUserAgents')]
+    public function testItDetectsLlmCrawlersByUserAgent(LlmCrawler $crawler, string $userAgent): void
     {
         $llmDetection = new LlmDetection();
-        self::assertSame($crawler, $llmDetection->getLlmCrawler(Request::create('/', server: ['HTTP_USER_AGENT' => "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko); compatible; $userAgent/1.1; +https://example.com"])));
+        $request = Request::create('/', server: ['HTTP_USER_AGENT' => "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko); compatible; $userAgent/1.1; +https://example.com"]);
+        self::assertSame($crawler, $llmDetection->getLlmCrawler($request));
+    }
+
+    /**
+     * @param list<string> $ipAddresses
+     */
+    #[DataProvider('crawlerProviderIpAddress')]
+    public function testItDetectsLlmCrawlersByIpAddress(LlmCrawler $crawler, array $ipAddresses): void
+    {
+        $llmDetection = new LlmDetection();
+        foreach ($ipAddresses as $ipAddress) {
+            $request = Request::create('/', server: ['REMOTE_ADDR' => $ipAddress]);
+            self::assertSame($crawler, $llmDetection->getLlmCrawler($request));
+        }
     }
 
     public function testItDoesNotDetectRegularClients(): void
     {
         $llmDetection = new LlmDetection();
-        self::assertNull($llmDetection->getLlmCrawler(Request::create('/', server: ['HTTP_USER_AGENT' => 'Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0'])));
+
+        $ipAddresses = [
+            '127.0.0.1',
+            '203.0.113.5', // RFC 5737 section 3 compliant
+        ];
+
+        foreach ($ipAddresses as $ipAddress) {
+            self::assertNull($llmDetection->getLlmCrawler(Request::create('/', server: [
+                'HTTP_USER_AGENT' => null,
+                'REMOTE_ADDR' => $ipAddress,
+            ])));
+
+            self::assertNull($llmDetection->getLlmCrawler(Request::create('/', server: [
+                'HTTP_USER_AGENT' => 'Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0',
+                'REMOTE_ADDR' => $ipAddress,
+            ])));
+        }
     }
 }
