65c4033a4632d3dd5a18382c3883650e02a89f41
Jérôme Deuchnord <git@deuchnord.fr>
2025-12-05 13:31:56 +0100

    Add Symfony Tests

---
+++ HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Deuchnord\NoAiBundle bundle.
+ *
+ * (c) Jérôme Deuchnord <jerome@deuchnord.fr>
+ *
+ * Licensed under the EUPL-1.2-or-later
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response('OK');
+    }
+}
